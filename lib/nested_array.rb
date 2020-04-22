7d2fe90f53fed9d393c1b17f4b4e6a3dc628ff10
Kosi Akporji <kosiazom@gmail.com>
2020-04-22 21:09:24 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,6 +36,8 @@ end
 def matrix_lookup(matrix, row, column)
   
    matrix_lookup[matrix][row][column],
+  end 
+   matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
